a31ed0f9bfd93bbdcdf2ab9075bb4ce7351a0f76
outsparkled <83491180+outsparkled@users.noreply.github.com>
2022-12-04 15:19:50 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,24 @@
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
-	name: "WebUntis",
-    majorVersion: 1,
-    minor: 1,
-	dependencies: [
-		.Package(url: "https://github.com/google/promises.git", majorVersion: 1, minor: 2),
-		.Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5, minor: 2),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 5, minor: 4),
-		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 1, minor: 3),
-	]
+    name: "WebUntis",
+    platforms: [
+        .macOS(.v10_12),
+            .iOS(.v10),
+            .tvOS(.v10),
+            .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "WebUntis", targets: ["WebUntis"])
+    ],
+    
+    dependencies: [
+        .package(url: "https://github.com/google/promises.git", from: "1.2.10"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.2"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.4.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1"),
+    ],
+    targets: [.target(name: "WebUntis", path: "Sources")]
 )
